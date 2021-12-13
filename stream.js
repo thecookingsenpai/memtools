170674b923aca3006184a7a1b2ce928132bc4ef0
thecookingsenpai <frassineticristiano@gmail.com>
2021-12-13 12:38:58 +0000

    append

--- stream.js
+++ stream.js
@@ -55,7 +55,7 @@ var init = function () {
       // This allows to track wallets
 		} else if(operation=="follow") {
         if(tx.from.toLowerCase()==args[1].toLowerCase() || tx.to.toLowerCase()==args[1].toLowerCase()) {
-          fs.writeFileSync('tracking/' + args[1], tx + "\n==================\n");
+          fs.writeFileSync('tracking/' + args[1], tx.toString() + "\n==================\n", { flag: 'a+' });
           console.log(tx);
 		    }
 		}
